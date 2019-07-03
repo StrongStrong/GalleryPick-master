f0bd991d8ab9de85d55757937c835adc21ddea37
qiang <91aqq@sina.com>
2019-07-03 22:30:24 +0800

    camere

---
+++ app/src/main/java/com/codergorilla/gallerypickdemo/BaseApplication.java
@@ -0,0 +1,38 @@
+package com.codergorilla.gallerypickdemo;
+
+import android.app.Application;
+
+/**
+ * BaseApplication
+ * Created by Yancy on 2016/1/27.
+ */
+public class BaseApplication extends Application {
+
+    private final static String TAG = "BaseApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+/*
+ *   ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ *     ┃　　　┃
+ *     ┃　　　┃
+ *     ┃　　　┗━━━┓
+ *     ┃　　　　　　　┣┓
+ *     ┃　　　　　　　┏┛
+ *     ┗┓┓┏━┳┓┏┛
+ *       ┃┫┫　┃┫┫
+ *       ┗┻┛　┗┻┛
+ *        神兽保佑
+ *        代码无BUG!
+ */
